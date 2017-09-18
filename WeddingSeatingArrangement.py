f4831249140ed9ada213cb531be0c1126e7653ac
Siri Gowda <sirigowd@usc.edu>
2017-09-18 12:16:36 -0700

    Update WeddingSeatingArrangement.py

--- WeddingSeatingArrangement.py
+++ WeddingSeatingArrangement.py
@@ -1,14 +1,12 @@
-import random
 import copy
+import externalproperties
 
 clauses = []
 cnf = []
+numguests = input[0]
+maxtables = input[1]
 
-def printclauses(clauses):
-    for clause in clauses:
-        print clause
-
-# ASSIGNMENTS AT ONLY ONE TABLE FOR EACH GUEST
+# Assigns one table per guest
 def onetableperguest():
     for guest in range(1, int(numguests) + 1):
         onetableclause = []
@@ -16,11 +14,8 @@ def onetableperguest():
             literal = 'X' + str(guest) + '>' + str(table)
             onetableclause.append(literal)
         clauses.append(onetableclause)
-
     for guest in range(1, int(numguests) + 1):
-
         for table1 in range(1, int(maxtables) + 1):
-
             for table2 in range(table1 + 1, int(maxtables) + 1):
                 if table1 < table2:
                     onetableclause = []
@@ -29,10 +24,9 @@ def onetableperguest():
                     literal2 = '~X' + str(guest) + '>' + str(table2)
                     onetableclause.append(literal2)
                     clauses.append(onetableclause)
-                    # printclauses(clauses)
 
-
-# FRIENDS SEATING
+# Implements friends seating constraints 
+# Friends must be seated next to each other
 def friendconstraints(rel):
     for table1 in range(1, int(maxtables) + 1):
         for table2 in range(1, int(maxtables) + 1):
@@ -43,10 +37,9 @@ def friendconstraints(rel):
                 literal2 = '~X' + rel[1] + '>' + str(table2)
                 onetableclause.append(literal2)
                 clauses.append(onetableclause)
-                # printclauses(clauses)
 
-
-# ENEMIES SEATING
+# Implements enemy constraints
+# Enemies must be seated away from each other
 def enemyconstraints(rel):
     for table in range(1, int(maxtables) + 1):
         onetableclause = []
@@ -55,8 +48,6 @@ def enemyconstraints(rel):
         literal2 = '~X' + rel[1] + '>' + str(table)
         onetableclause.append(literal2)
         clauses.append(onetableclause)
-        # printclauses(clauses)
-
 
 def getsymbolsinsentencedpll(clauses):
     symbols = []
@@ -65,7 +56,6 @@ def getsymbolsinsentencedpll(clauses):
             if literal not in symbols:
                 symbols.append(literal)
     return symbols
-
 
 def removefromsymbols(symbols, toberemovedsymbol):
     newsymlist = copy.copy(symbols)
@@ -79,25 +69,21 @@ def removefromsymbols(symbols, toberemovedsymbol):
             newsymlist.remove("~" + toberemovedsymbol)
     return newsymlist
 
-
 def dpll(clauses, symbols, model):
     if bool(model):
         if evaluateassignmentdpll(clauses, model):
             return model
-
         # returns only if some clause returns false(not if it returns None or true)
         for clause in clauses:
             value = findvaluesofclausedpll(clause, model)
             if (value != None) and (not value):
                 return False
-
     symbolValue = findPureSymboldpll(clauses, symbols, model)
     if bool(symbolValue):
         symbolvaluepair = next(iter(symbolValue))
         return dpll(clauses, removefromsymbols(symbols, symbolvaluepair),
                     modelunion(model, symbolvaluepair, symbolValue[symbolvaluepair]))
         # make consistant by also assigning for corresponding negation of that
-
     symbolValue = findunitclause(clauses, model)
     if bool(symbolValue):
         symbolvaluepair = next(iter(symbolValue))
@@ -107,10 +93,9 @@ def dpll(clauses, symbols, model):
     rest = symbols
     return dpll(clauses, removefromsymbols(rest, first), modelunion(model, first, True)) or dpll(clauses,removefromsymbols(rest, first), modelunion(model, first, False))
 
-
+# returns false if even one completely assigned clause evaluates to false
+# returns true otherwise
 def evaluateassignmentdpll(clauses, model):
-    # returns false if even one completely assigned clause evaluates to false
-    # returns true otherwise
     for clause in clauses:
         cvalue = findvaluesofclausedpll(clause, model)
         # if (cvalue!=None) and (not cvalue):
@@ -118,43 +103,34 @@ def evaluateassignmentdpll(clauses, model):
             return False
     return True
 
-
+# returns None if a literal in the clause is unassigned
+# returns true if clause evaluates to true
 def findvaluesofclausedpll(clause, model):
-    # returns None if a literal in the clause is unassigned
-    # returns true if clause evaluates to true, false other
     value = None
     unassigned = False
     # for positive literal
     for literal in [x for x in clause if not "~" in x]:
-        if bool(model):
-            if literal in model:
-                if model[literal]:
-                    value = True
-                    break
-
-            else:
-                unassigned = True
+        if bool(model) & (literal in model):
+            if model[literal]:
+                value = True
+                break
         else:
             unassigned = True
     # for negetive literals
     if value == None:
         for literal in filter(lambda x: '~' in x, clause):
-            if bool(model):
-                # if literal.strip('~') in model:
-                if literal in model:
-                    # if not model[literal.strip('~')]:
-                    if model[literal]:
-                        value = True
-                        break
-                else:
-                    unassigned = True
+            # if literal.strip('~') in model:
+            if bool(model) & (literal in model):
+                # if not model[literal.strip('~')]:
+                if model[literal]:
+                    value = True
+                    break
             else:
                 unassigned = True
         if value == None:
             if not unassigned:
                 value = False
     return value
-
 
 def modelunion(model, symbol, value):
     newmodel = copy.copy(model)
@@ -165,7 +141,6 @@ def modelunion(model, symbol, value):
         newmodel[symbol] = value
         newmodel["~" + symbol] = not value
     return newmodel
-
 
 def findPureSymboldpll(clauses, symbols, model):
     result = {}
@@ -198,7 +173,6 @@ def findPureSymboldpll(clauses, symbols, model):
         result[pureneg] = True
     return result
 
-
 def findunitclause(clauses, model):
     # returns unit clause symbol and value if one exists
     # else returns {}
@@ -224,60 +198,55 @@ def findunitclause(clauses, model):
                 else:
                     result[unassigned] = True
                     break
-
     return result
 
+def printclauses(clauses):
+    for clause in clauses:
+        print clause
 
-# Read input
-#rawinput = open('input.txt', 'r')
-rawinput = open('C:/aitest/testothers.txt', 'r')
-lines = rawinput.read().splitlines()
-input = lines[0].split()
-numguests = input[0]
-maxtables = input[1]
-#f = open('output.txt', 'w')
-f = open('C:/aitest/output.txt', 'w')
-if int(numguests)<=0:
-    #f = open('C:/aitest/output.txt', 'w')
-    print >> f, "yes"
-elif int(numguests)>0 and int(maxtables)<=0:
-    #f = open('C:/aitest/output.txt', 'w')
-    print >> f, "no"
-else:
+def get_seat_arrangement(f, lines, numguests):
     lines.pop(0)
     onetableperguest()
-
     for line in lines:
         rel = line.split()
         if rel[2] == 'F':
             friendconstraints(rel)
         elif rel[2] == 'E':
             enemyconstraints(rel)
-
-    result=dpll(clauses, getsymbolsinsentencedpll(clauses), {})
-
-    #f = open('C:/aitest/output.txt', 'w')
-
+    result = dpll(clauses, getsymbolsinsentencedpll(clauses), {})
     if result:
         print >> f, "yes"
-        assigngtot = []
-        assignment={}
+        assignment = {}
         for item in result:
-            if result[item]:
-                if "~" not in item:
-                    gt = item.strip('X').split('>')
-                    assignment[gt[0]] = gt[1]
-                    guesttable = item.strip('X').split('>')
-
-        if len(assignment)<int(numguests):
-            guest=1
-            while guest<int(numguests)+1:
+            if result[item] & ("~" not in item):
+                gt = item.strip('X').split('>')
+                assignment[gt[0]] = gt[1]
+        if len(assignment) < int(numguests):
+            guest = 1
+            while guest < int(numguests) + 1:
                 if str(guest) not in assignment:
-                    assignment[str(guest)]="1"
-                guest+=1
-
-        for guest in sorted(assignment.keys(),key = int):
+                    assignment[str(guest)] = "1"
+                guest += 1
+        for guest in sorted(assignment.keys(), key=int):
             print >> f, guest, assignment[guest]
-
     else:
         print >> f, "no"
+
+
+def main():
+    # Read input from INPUT_FILE_PATH
+    rawinput = open(externalproperties.INPUT_FILE_PATH, 'r')
+    lines = rawinput.read().splitlines()
+
+    # Print output to OUTPUT_FILE_PATH
+    f = open(externalproperties.OUTPUT_FILE_PATH, 'w')
+
+    if int(numguests)<=0:
+        print >> f, "yes"
+    elif int(numguests)>0 and int(maxtables)<=0:
+        print >> f, "no"
+    else:
+        get_seat_arrangement(f, lines, numguests)
+
+if __name__ == "__main__":
+    main()
